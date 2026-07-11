ed1bdb287b70150ec2e3c60764b04a49941d8aa4
AutoSharmMain <kyskitsune@gmail.com>
2026-07-11 23:57:26 +0300

    fix: set unoptimized true for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true, 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rcpfezkozgxkrvjjtluy.supabase.co',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
